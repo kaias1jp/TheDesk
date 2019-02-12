fcbb1e6e19e89ebb6b903eace33b60f4aee4a38d
popn.ja <popn.ja@gmail.com>
2019-02-12 16:46:55 +0900

    modified:   img.js

--- app/js/post/img.js
+++ app/js/post/img.js
@@ -63,22 +63,234 @@ function closedrop() {
 function fileselect() {
 	ipc.send('file-select', "");
 }
+function readImage(file) {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.onload = () => resolve(image);
+image.onerror = (e) => reject(e);
+image.src = file;
+});
+}
+
+
+function readFile(file) {
+    return new Promise((resolve, reject) => {
+        const fr = new FileReader();
+        fr.onload = (event) => resolve(event.target.result);
+fr.onerror = (e) => reject(e);
+        fr.readAsDataURL(file);
+});
+}
+
+
+function getFileContentAsBase64(path,callback){
+    window.resolveLocalFileSystemURL(path, gotFile, fail);
+            
+    function fail(e) {
+          alert('Cannot found requested file');
+    }
+
+    function gotFile(fileEntry) {
+           fileEntry.file(function(file) {
+              var reader = new FileReader();
+              reader.onloadend = function(e) {
+                   var content = this.result;
+                   callback(content);
+              };
+              // The most important point, use the readAsDatURL Method from the file plugin
+              reader.readAsDataURL(file);
+           });
+    }
+}
+
+function convertImgToBase64()
+{
+    var canvas = document.createElement('CANVAS');
+    img = document.createElement('img'),
+    img.src = "/tmp/temp.jpg";
+    img.onload = function()
+    {
+        canvas.height = img.height;
+        canvas.width = img.width;
+        b64 = canvas.toDataURL('image/png');
+        canvas = null; 
+    };
+}
+
+
+
 
 //ファイル読み込み
 function handleFileUpload(files, obj, no) {
-	var fr = new FileReader();
-	fr.onload = function(evt) {
-		var b64 = evt.target.result;
+console.log("file upload");
+//alert("1upload:"+files["name"]+files["type"]);
+//	var fr = new FileReader();
+//	fr.onload = function(evt) {
+
+/*readFile(files).then(evt=> {
+
+
+
+
+var b64 = evt.result;
+
+//alert("2start resize");
+
+});*/
+var w = 0; 
+var h = 0;
+var b64 = "";
+readImage(files.path).then(image=> {
+
+result = {width: image.naturalWidth, height: image.naturalHeight};
+
+//alert("3image:width:"+result.width);
+if (result.width*result.height>1024*1024) {
+
+if (result.width>result.height) {
+w = 1280;
+h = result.height*(1280.0/result.width);
+} else {
+h = 1280;
+w = result.width*(1280.0/result.height);
+}
+}
+//alert("4w:"+w+" h:"+h);
+
+//alert("in jimp"+w+" "+h);
+var Jimp = require("jimp");
+ Jimp.read(files["path"])
+    .then(image=>{
+     image
+       .resize(w,h)
+         .getBase64(Jimp.MIME_JPEG, function (err, src) {
+                $('#b64-box').val(src);
+                var ret = media(src, files["type"], no, true);
+       
+
+
+});
+}).catch(function (err) {
+    alert(err);
+});
+
+
+
+
+
+
+
+
+
+
+}).catch(e=> {
+alert(e);
+});
+
+
+
+/*var image = new Image();
+image.src =fr.result;
+image.onload = function() {
+
+result = {width: image.naturalWidth, height: image.naturalHeight};
+
+//alert("image:width:"+result.width);
+if (result.width*result.height>1024*1024) {
+
+if (result.width>result.height) {
+w = 1280;
+h = result.height*(1280.0/result.width);
+} else {
+h = 1280;
+w = result.width*(1280.0/result.height);
+}
+
+
+//alert("w:"+w+" h:"+h);
+}
+}*/
+readFile(files).then(result=> {
+//alert("readFile");
+if (w*h>0) {
+//alert("img64");
+/*   ImgB64Resize(result, w, h).then(img_b64=>{
+            // Destination Image
+//alert("re media()");
+       b64 = img_b64;
+}).catch(e=>{
+alert(e);
+
+
+
+});*/
+var b64 = "";
+/*alert("in jimp"+w+" "+h);
+var Jimp = require("jimp");
+ Jimp.read(files["path"])
+    .then(image=>{
+     image
+       .resize(w,h)
+.getBase64(Jimp.MIME_PNG, function (err, src) {
+            mainWindow.webContents.send('bmp-img-comp', [src,0]);
+     });
+       .getBase64Async(Jimp.AUTO).then(base64=>{
+//alert("getbase64");              
+b64= base64;
+         });
+}).catch(function (err) {
+    alert(err);
+});*/
+/*       .getBase64ASync(Jimp.MIME_JPEG)
+       .then(base64Image => b64 = base64Image)
+//       .catch(err => ...)
+})*/
+//.catch(err => ...)
+/*readFile("/tmp/temp.jpg").then(result=> {
+b64 = result;
+//alert("out jimp");
+}).catch(e=>{
+alert(e);
+
+
+});*/
+/*getFileContentAsBase64("/tmp/temp.jpg",function(b64){
+  //window.open(base64Image);
+  console.log(b64); 
+  // Then you'll be able to handle the myimage.png file as base64
+});*/
+
+//alert("out jimp:");
+//alert("media call2");
+                $('#b64-box').val(b64);
+                var ret = media(b64, files["type"], no, true)
+
+//alert("end");
+
+}else{ 
+
+
+b64 = result;
+//}
+//alert("media call");
 		$('#b64-box').val(b64);
-		var ret = media(b64, files["type"], no)
+		var ret = media(b64, files["type"], no, false)
 	}
-	fr.readAsDataURL(files);
+//	fr.readAsDataURL(files);
+}).catch(e=> {
+alert(e);
+
+
+});
+
+
 	$("#mec").append(files["name"] + "/");
 }
 
 //ファイルアップロード
-function media(b64, type, no) {
-	var l = 4;
+function media(b64, type, no, isblob) {
+//alert("media start");
+var l = 4;
 	var c = "abcdefghijklmnopqrstuvwxyz0123456789";
 	var cl = c.length;
 	var r = "";
@@ -92,15 +304,22 @@ function media(b64, type, no) {
 	}
 	$(".toot-btn-group").prop("disabled", true);
 	$("#post-acct-sel").prop("disabled", true);
+//alert("toblob");
 	localStorage.setItem("image","busy");
 	todo("Image Upload...");
+if (isblob != true){
 	var media = toBlob(b64, type);
-	console.log(media);
+}else{
+//alert(b64);
+var media = toBlob(b64,type);
+}
+//	alert(media.size);
 	var fd = new FormData();
 	fd.append('file', media);
 	var acct_id = $("#post-acct-sel").val();
 	var domain = localStorage.getItem("domain_" + acct_id);
 	var at = localStorage.getItem("acct_"+ acct_id + "_at");
+//alert(acct_id);
 	var httpreq = new XMLHttpRequest();
 	if(localStorage.getItem("mode_" + domain)=="misskey"){
 		var start = "https://" + domain + "/api/drive/files/create";
@@ -124,12 +343,105 @@ function media(b64, type, no) {
 		httpreq.responseType = 'json';
 		httpreq.setRequestHeader('Authorization', 'Bearer ' + at);
 		httpreq.send(fd);
+//alert("start:"+start);
 	}
-    httpreq.onreadystatechange = function() {
+//    httpreq.onreadystatechange = function() {
+//alert(JSON.parse(httpreq.response));
+//alert("state:"+httpreq.readyState);
+httpreq.onload = function() {
+//alert(httpreq.status);
+/*if (httpreq.status == 422) {
+//alert("media resize check w:"+w+" h:"+h);
+    ImgB64Resize(b64, w, h, 
+        function(img_b64) {
+            // Destination Image
+//alert("re media()");
+       var media = toBlob(img_b64, type);
+        //alert(media.size);
+        var fd = new FormData();
+        fd.append('file', media);
+        var acct_id = $("#post-acct-sel").val();
+        var domain = localStorage.getItem("domain_" + acct_id);
+        var at = localStorage.getItem("acct_"+ acct_id + "_at");
+//alert(acct_id);
+        var httpreq = new XMLHttpRequest();
+        if(localStorage.getItem("mode_" + domain)=="misskey"){
+                var start = "https://" + domain + "/api/drive/files/create";
+                httpreq.open('POST', start, true);
+                httpreq.upload.addEventListener("progress", progshow, false);
+                httpreq.responseType = 'json';
+                if ($("#nsfw").hasClass("nsfw-avail")) {
+                        var nsfw = true;
+                } else {
+                        var nsfw = false;
+                }
+                var previewer="url"
+                fd.append('i', at);
+                //fd.append('isSensitive', nsfw);
+                httpreq.send(fd);
+        }else{
+                var previewer="preview_url"
+                var start = "https://" + domain + "/api/v1/media";
+                httpreq.open('POST', start, true);
+                httpreq.upload.addEventListener("progress", progshow, false);
+                httpreq.responseType = 'json';
+                httpreq.setRequestHeader('Authorization', 'Bearer ' + at);
+                httpreq.send(fd);
+}
+httpreq.onload = function() {
+//alert(httpreq.status);
+//alert(httpreq.statusText);
+                if (httpreq.readyState == 4) {
+//alert("state:4");
+                        var json = httpreq.response;
+                        var img = localStorage.getItem("img");
+//alert(json.id);
+//alert(json.type);
+//alert(json.url);
+                        if (json.type.indexOf("image")!=-1) {
+                                var html = '<img src="' + json[previewer] + '" style="width:50px; max-height:100px;">';
+                                $('#preview').append(html);
+                        } else {
+                                $('#preview').append(lang.lang_postimg_previewdis);
+                        }
+                        if (!img) {
+                                var img = "no-act";
+                        }
+                        if (img != "inline") {
+                                var mediav=$("#media").val();
+                                var regExp = new RegExp("tmp_"+r, "g");
+                                mediav = mediav.replace(regExp, json["id"]);
+                                $("#media").val(mediav);
+
+                        }
+                        if (img == "url") {
+                                $("#textarea").val($("#textarea").val() + " " + json["text_url"])
+                        }
+                        todc();
+                        $(".toot-btn-group").prop("disabled", false);
+                        $('select').material_select();
+                        $("#mec").text(lang.lang_there);
+                        Materialize.toast(lang.lang_postimg_aftupload, 1000);
+                        $("#imgup").text("");
+                        $("#imgsel").show();
+                        localStorage.removeItem("image");
+                }
+        }
+
+        }
+
+    );
+
+
+}*/
+//alert(httpreq.statusText);
 		if (httpreq.readyState == 4) {
+//alert("state:4");
 			var json = httpreq.response;
-			console.log(json);
 			var img = localStorage.getItem("img");
+//alert(json.id);
+//alert(json.type);
+//alert(json.url);
 			if (json.type.indexOf("image")!=-1) {
 				var html = '<img src="' + json[previewer] + '" style="width:50px; max-height:100px;">';
 				$('#preview').append(html);
@@ -160,9 +472,68 @@ function media(b64, type, no) {
 		}
 	}
 }
+function ImgB64Resize(imgB64_src, width, height) {
+  return new Promise((resolve, reject) => {
+   var img_type = imgB64_src.substring(5, imgB64_src.indexOf(";"));
+    const img = new Image();
+    img.onload = (imgB64_dst) => {
+        // New Canvas
+        var canvas = document.createElement('canvas');
+        canvas.width = width;
+        canvas.height = height;
+        // Draw (Resize)
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0, width, height);
+        // Destination Image
+        imgB64_dst = canvas.toDataURL(img_type);
+}
+    img.onerror = (e) => reject(e);
+  img.src = imgB64_src;
+});
+}
+/*    // Image Type
+    var img_type = imgB64_src.substring(5, imgB64_src.indexOf(";"));
+    // Source Image
+    var img = new Image();
+    img.onload = function() {
+        // New Canvas
+        var canvas = document.createElement('canvas');
+        canvas.width = width;
+        canvas.height = height;
+        // Draw (Resize)
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0, width, height);
+        // Destination Image
+        var imgB64_dst = canvas.toDataURL(img_type);
+        callback(imgB64_dst);
+    };
+    img.src = imgB64_src;
+}*/
 
 //Base64からBlobへ
 function toBlob(base64, type) {
+var bin = atob(base64.replace(/^.*,/, ''));
+var buffer = new ArrayBuffer(bin.length);
+var dv = new DataView(buffer);
+for (var i = 0; i < bin.length; i++) {
+ dv.setUint8(i, bin.charCodeAt(i));
+}
+//alert(bin.length);
+        try {
+                var blob = new Blob([buffer], {
+                        type: type
+                });
+        } catch (e) {
+//alert("blob error");
+                return false;
+        }
+
+        return blob;
+
+
+}
+
+function atoBlob(base64, type) {
 	var bin = atob(base64.replace(/^.*,/, ''));
 	var buffer = new Uint8Array(bin.length);
 	for (var i = 0; i < bin.length; i++) {
@@ -170,15 +541,17 @@ function toBlob(base64, type) {
 	}
 	// Blobを作成
 	try {
-		var blob = new Blob([new Uint8Array(buffer)], {
+		var blob = new Blob([buffer], {
 			type: type
 		});
 	} catch (e) {
+//alert("blob error");
 		return false;
 	}
 
 	return blob;
 }
+
 //画像を貼り付けたら…
 var element =  document.querySelector("#textarea");
 element.addEventListener("paste", function(e){
